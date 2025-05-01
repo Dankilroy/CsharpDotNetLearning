1d56d4bfe79d871402fa70f27a36f0c39849d6e3
Dan <44769240+Dankilroy@users.noreply.github.com>
2025-05-01 08:59:53 +0100

    commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//specify the data source
+List<int> scores = [75, 89, 95, 77, 82];
+
+
+
+// Define the query expression
+IEnumerable<int> scoreQuery =
+from score in scores
+where score > 80
+select score;
+
+// Exuecute the query and display the results
+foreach (int i in scoreQuery)
+{
+    Console.WriteLine(i + " ");
+}
+
